99334e2b42933e8eca371764e8608ec565a8011e
Rajkumar <44458957+rajkumarj-git@users.noreply.github.com>
2020-05-05 19:14:15 +0530

    html, css, scss, script
    created slave for first cut version

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
-
-  $("#submit").click(function(){
-    $("#guide").removeClass("hide");
-    $("#guide-disabled").addClass("hide");
-    
-    
-
-
-  });
