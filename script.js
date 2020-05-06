2fa20fc293fd0d44517577c536de08ef87e56b44
Rajkumar <44458957+rajkumarj-git@users.noreply.github.com>
2020-05-06 17:46:00 +0530

    adding angularjs

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ $('#myModal').on('shown.bs.modal', function () {
     $("#guide").removeClass("hide");
     $("#guide-disabled").addClass("hide");
     
-    
+    });
 
+    var app = angular.module('myApp', [])
 
-  });
+  app.controller('myController', function($scope) {
+
+   var name, place;
+
+    place= "chennai";
+    $scope.name="raj";
+    });
